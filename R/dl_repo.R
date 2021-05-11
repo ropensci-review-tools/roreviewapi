b1f062485152cd18fbd75b28e7b3f23cae91b3a8
mpadge <mark.padgham@email.com>
2021-05-11 16:03:37 +0200

    fix cache_dir vars in dl_gh_repo

--- R/dl_repo.R
+++ R/dl_repo.R
@@ -8,11 +8,11 @@ dl_gh_repo <- function (u) {
     repo <- utils::tail (strsplit (u, "/") [[1]], 1)
     org <- utils::tail (strsplit (u, "/") [[1]], 2) [1]
 
-    cache_dir <- Sys.getenv ("cache_dir")
-    repo_updated <- roreviewapi::check_cache (org, repo, cache_dir)
+    cache_dir <- Sys.getenv ("pkgcheck_cache_dir")
+    repo_updated <- check_cache (org, repo, cache_dir)
     branch <- pkgcheck::get_default_branch (org, repo)
 
-    download_repo <- function (u, repo, branch) {
+    clone_repo <- function (u, repo, branch) {
 
         if (substring (u, nchar (u), nchar (u)) == "/")
             u <- substring (u, 1, nchar (u) - 1)
@@ -21,8 +21,6 @@ dl_gh_repo <- function (u) {
         f <- file.path (cache_dir, repo)
         if (file.exists (f))
             chk <- unlink (f, recursive = TRUE)
-        #f <- file.path (cache_dir, paste0 (repo, "-", branch, ".zip"))
-        #utils::download.file (u, destfile = f)
         gert::git_clone (url = u, path = f)
         return (f)
     }
@@ -32,7 +30,7 @@ dl_gh_repo <- function (u) {
     if (!file.exists (f) | repo_updated) {
 
         message ("Cloning repo ...")
-        f <- download_repo (u, repo, branch) # nolint
+        f <- clone_repo (u, repo, branch)
     }
 
     return (f)
