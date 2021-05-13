648f4b520891e3b656a6a4a174595ad5adce8488
mpadge <mark.padgham@email.com>
2021-05-13 16:50:03 +0200

    review plumber endpoint

--- inst/plumber.R
+++ inst/plumber.R
@@ -1,140 +1,5 @@
 # plumber.R
 
-# --------------------------------------------------
-# ------------------   network   ------------------
-# --------------------------------------------------
-
-#* Return report from package URL
-#* @param u The URL for a repo
-#* @post /network
-function (u) {
-
-    cache_dir <- Sys.getenv ("cache_dir")
-    local_repo <- pkgcheck::dl_gh_repo (u)
-    local_zip <- paste0 (local_repo, ".zip")
-    flist <- unzip (local_zip, exdir = cache_dir)
-
-    # check and modify permissions of configure files
-    if (any (list.files (local_repo) == "configure")) {
-        cmd <- paste0 ("chmod +755 ", file.path (local_repo, "configure"))
-        system (cmd)
-    }
-
-    visjs_dir <- file.path (cache_dir, "static") # in api.R
-    repo <- tail (strsplit (u, "/") [[1]], 1)
-    org <- tail (strsplit (u, "/") [[1]], 2) [1]
-    commit <- pkgcheck::get_latest_commit (org, repo)
-    oid <- substring (commit$oid, 1, 8)
-    visjs_file <- paste0 (repo, "_pkgstats", oid, ".html")
-
-    # clean up any older ones
-    flist <- list.files (visjs_dir,
-                         pattern = paste0 (repo, "_pkgstats"),
-                         full.names = TRUE)
-    unlink (flist, recursive = TRUE)
-
-    s <- pkgstats::pkgstats (local_repo)
-    visjs_path <- file.path (visjs_dir, visjs_file)
-    pkgstats::plot_network (s, vis_save = visjs_path)
-
-    visjs_url <- paste0 (Sys.getenv ("pkgcheck_url"), "/assets/", visjs_file)
-
-    out <- c ("### Network visualisation",
-              "",
-              paste0 ("[Click here](",
-                      visjs_url,
-                      ") for interactive network visualisation ",
-                      "of calls between objects in package."))
-
-    return (paste0 (out, collapse = "\n"))
-}
-
-# --------------------------------------------------
-# ---------------------   gp   ---------------------
-# --------------------------------------------------
-
-#* Return goodpractice results from a package URL
-#* @param u The URL for a repo
-#* @post /gp
-function (u) {
-
-    cache_dir <- Sys.getenv ("cache_dir")
-    local_repo <- pkgcheck::dl_gh_repo (u)
-    local_zip <- paste0 (local_repo, ".zip")
-    flist <- unzip (local_zip, exdir = cache_dir)
-
-    # check and modify permissions of configure files
-    if (any (list.files (local_repo) == "configure")) {
-        cmd <- paste0 ("chmod +755 ", file.path (local_repo, "configure"))
-        system (cmd)
-    }
-
-    # pkgcheck_install_deps is called in get_gp_report
-    gp <- pkgcheck::get_gp_report (u, local_repo)
-
-    message ("unlinking ", local_repo)
-    chk <- unlink (local_repo, recursive = TRUE)
-
-    control <- list (cyclocomp_threshold = 15,
-                     covr_threshold = 70,
-                     digits = 2)
-
-    res <- pkgcheck::process_gp (gp,
-                                  control = control)
-
-    res <- paste0 (res, collapse = "\n")
-
-    return (res)
-}
-
-# --------------------------------------------------
-# -------------------   check   --------------------
-# --------------------------------------------------
-
-#* Return srr results from a package URL
-#* @param u The URL for a repo
-#* @post /check
-function (u) {
-
-    cache_dir <- Sys.getenv ("cache_dir")
-    local_repo <- pkgcheck::dl_gh_repo (u)
-    local_zip <- paste0 (local_repo, ".zip")
-    flist <- unzip (local_zip, exdir = cache_dir)
-
-    chk <- pkgcheck::editor_check (local_repo, u)
-
-    junk <- unlink (local_repo, recursive = TRUE)
-
-    return (chk)
-}
-
-# --------------------------------------------------
-# --------------------   srr   ---------------------
-# --------------------------------------------------
-
-#* Return srr results from a package URL
-#* @param u The URL for a repo
-#* @post /srr
-function (u) {
-
-    cache_dir <- Sys.getenv ("cache_dir")
-    local_repo <- pkgcheck::dl_gh_repo (u)
-    local_zip <- paste0 (local_repo, ".zip")
-    flist <- unzip (local_zip, exdir = cache_dir)
-
-    res <- tryCatch (
-                srr::srr_stats_pre_submit (local_repo, quiet = TRUE),
-                error = function (e) e)
-    if (methods::is (res, "error")) {
-        res <- paste0 ("- ", res$message)
-    }
-
-    res <- paste0 (res, collapse = "\n")
-
-    junk <- unlink (local_repo, recursive = TRUE)
-
-    return (res)
-}
 
 # --------------------------------------------------
 # ----------------   editorcheck   -----------------
@@ -150,97 +15,51 @@ function (u) {
     local_zip <- paste0 (local_repo, ".zip")
     flist <- unzip (local_zip, exdir = cache_dir)
 
-    ec <- pkgcheck::pkgcheck (local_repo)
-    ec <- editor_check (local_repo)
+    check <- roreviewapi::editor_check (path)
+    a <- attributes (check)
+    u <- roreviewapi::push_to_gh_pages (check)
+    check <- strsplit (check, "\n") [[1]]
+    check <- gsub (a$network_file, u [1], check)
+    check <- gsub (a$srr_report_file, u [2], check)
 
-    srr <- pkgcheck::pkgcheck_srr_report (local_repo)
-    srr_okay <- attr (srr, "srr_okay")
+    eic_instr <- c ("",
+                    "---",
+                    "",
+                    "## Editor-in-Chief Instructions:",
+                    "")
 
-    gp <- check <- NULL
-    is_noteworthy <- FALSE
-    eic_instr <- c ("", "## Editor-in-Chief Instructions:", "")
-
-    if (srr_okay) {
-
-        # check and modify permissions of configure files
-        if (any (list.files (local_repo) == "configure")) {
-            cmd <- paste0 ("chmod +755 ", file.path (local_repo, "configure"))
-            system (cmd)
-        }
-
-        # pkgcheck_install_deps is called in get_gp_report
-        gp <- pkgcheck::get_gp_report (u, local_repo)
-
-        control <- list (cyclocomp_threshold = 15,
-                         covr_threshold = 70,
-                         digits = 2)
-
-        gp <- process_gp (gp, control = control)
-
-        gp <- c ("",
-                 "### goodpractice results",
-                 "",
-                 "",
-                 gp,
-                 "")
-
-        gp <- paste0 (gp, collapse = "\n")
-
-
-        check <- pkgcheck::editor_check (local_repo, u)
-        is_noteworthy <- attr (check, "is_noteworthy")
-
-        if (any (grepl (pkgcheck::symbol_crs (), check))) {
-
-            eic_instr <- c (eic_instr,
-                            paste0 ("Processing may not proceed until the ",
-                                    "items marked with ",
-                                    pkgcheck::symbol_crs (),
-                                    " have been resolved."))
-        } else {
-
-            eic_instr <- c (eic_instr,
-                            paste0 ("This package is in top shape and may ",
-                                    "be passed on to a handling editor"))
-        }
-
-    } else {
+    if (!a$srr_okay) {
 
         eic_instr <- c (eic_instr,
                         paste0 ("Processing may not proceed until the 'srr' ",
                                 "issues identified above have been adressed."))
     }
 
+    if (!a$checks_okay) {
+
+        eic_instr <- c (eic_instr,
+                        paste0 ("Processing may not proceed until the ",
+                                "items marked with ",
+                                roreviewapi::symbol_crs (),
+                                " have been resolved."))
+    } else {
+
+        eic_instr <- c (eic_instr,
+                        paste0 ("This package is in top shape and may ",
+                                "be passed on to a handling editor"))
+    }
+
     message ("unlinking ", local_repo)
     junk <- unlink (local_repo, recursive = TRUE)
 
-    # collapse core of check and gp into a details section:
-    chk_gp <- c (strsplit (check, "\n") [[1]],
-                 strsplit (gp, "\n") [[1]])
-
-    noteworthy <- ifelse (is_noteworthy,
+    noteworthy <- ifelse (a$is_noteworthy,
                           c (paste0 ("This package has some noteworthy ",
                                      "properties, see 'Package Statistics' ",
                                      "details below"),
                              ""),
                           "")
-    i <- grep ("\\#\\#\\# Package Statistics", chk_gp) - 1
-    chk_gp <- c (chk_gp [seq (i)],
-                 "",
-                 "### Details",
-                 "",
-                 noteworthy,
-                 "<details>",
-                 "<summary>Click to see</summary>",
-                 "<p>",
-                 "",
-                 chk_gp [-seq (i)],
-                 "",
-                 "</p></details>",
-                 "")
 
-
-    out <- paste0 (c (chk_gp, srr, eic_instr), collapse = "\n")
+    out <- paste0 (c (check, eic_instr), collapse = "\n")
 
 
     return (out)
