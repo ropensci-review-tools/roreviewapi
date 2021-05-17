e69948146bc8eb1e6d21fa6fa641071df23148b0
mpadge <mark.padgham@email.com>
2021-05-17 13:48:37 +0200

    lint man entry

--- R/editor-check.R
+++ R/editor-check.R
@@ -2,9 +2,11 @@
 #' Body of main 'editorcheck' response
 #'
 #' @param repourl The URL for the repo being checked
-#' @param repo The 'context.repo' parameter defining the repository from which the command was invoked.
+#' @param repo The 'context.repo' parameter defining the repository from which
+#' the command was invoked.
 #' @param issue_id The id of the issue form which the command was invoked
-#' @param post_to_issue Integer value > 0 will post results back to issue (via gh' cli); otherwise just return character string with result.
+#' @param post_to_issue Integer value > 0 will post results back to issue (via
+#' 'gh' cli); otherwise just return character string with result.
 #' @return If `!post_to_issue`, a markdown-formatted response body from static
 #' package checks, otherwise URL of the issue comment to which response body has
 #' been posted.
