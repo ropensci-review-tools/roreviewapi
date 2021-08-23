a734ef1e8cbd4cf8f7db06d710eb20e43c58ba1b
mpadge <mark.padgham@email.com>
2021-08-23 16:24:19 +0200

    add stats-badge fn for #9

--- R/srr.R
+++ R/srr.R
@@ -19,3 +19,45 @@ stats_version <- function () {
 
     return (v)
 }
+
+#' Get stats badge grade and standards version for a submission
+#'
+#' @param repo The submission repo
+#' @param issue_num GitHub issue number of submission
+#' @return A single character containing the label used directly for the issue
+#' badge
+#' @export
+stats_badge <- function (repo = "ropensci/software-review",
+                         issue_num = 258) {
+
+    # This by default returns only the opening comment. Additional comments can
+    # be extracted with the "-c" flag.
+    args <- list ("issue",
+                  "view",
+                  issue_num,
+                  "-R",
+                  repo)
+
+    out <- system2 ("gh", args = args, stdout = TRUE, wait = TRUE)
+
+    type <- get_html_var (out, "submission-type")
+    if (length (type) == 0L)
+        return (NULL)
+
+    grade <- get_html_var (out, "statsgrade")
+    version <- stats_version ()
+
+    if (type != "Stats")
+        return (NULL)
+
+    return (paste0 ("6\approved-", grade, "-v", version))
+}
+
+get_html_var <- function (x, expr = "submission-type") {
+
+    x <- grep (paste0 ("<!--", expr, "-->"), x, value = TRUE)
+    x <- regmatches (x, regexpr ("\\-\\->.*<!\\-\\-", x))
+    x <- gsub ("\\-\\->|<!\\-\\-", "", x)
+
+    return (x)
+}
