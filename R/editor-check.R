deb0999de70ba6e90d0451bd409856b49c9a1569
mpadge <mark.padgham@email.com>
2021-08-24 11:16:48 +0200

    add file_pkgcheck_issue; closes #11

--- R/editor-check.R
+++ R/editor-check.R
@@ -31,10 +31,13 @@ editor_check <- function (repourl, repo, issue_id, post_to_issue = TRUE) {
 
     } else {
 
-        out <- paste0 ("Oops, something went wrong with out automatic package ",
-                       "checks. Our developers have been notified, and a report ",
-                       "will appear here as soon as we've resolved the issue. ",
-                       "Sorry for any inconvenience!")
+        u <- file_pkgcheck_issue (repourl, repo, issue_id)
+
+        out <- paste0 ("Oops, something went wrong with our automatic package ",
+                       "checks. Our developers [have been notified](", u,
+                       ") and a report will appear here as soon as we've ",
+                       "resolved the issue. Sorry for any inconvenience!")
+
     }
 
     if (post_to_issue) {
@@ -109,4 +112,49 @@ collate_editor_check <- function (checks) {
     out <- paste0 (c (check, eic_instr), collapse = "\n")
 
     return (out)
+}
+
+#' File an issue in the GitHub `pkgcheck` repo on any packages which fail checks
+#'
+#' @noRd
+file_pkgcheck_issue <- function (repourl = NULL,
+                                 repo = "ropensci/software-review",
+                                 issue_id = NULL) {
+
+    user <- get_github_user ()
+
+    if (!user %in% authorized_users)
+        return (NULL)
+
+    if (grepl ("github", repo))
+        repo <- gsub ("https://github.com/", "", repo)
+
+    pkg_name <- utils::tail (strsplit (repourl, "/") [[1]], 1)
+    # Note that title has to be quoted. body is okay b/c read from file.
+    title <- paste0 ("'roreviewapi fail for ", pkg_name, " package'")
+    body <- paste0 ("Automatic checks failed for submission of ", pkg_name,
+                    " package.\n\n",
+                    "Package URL is [", 
+                    gsub ("^https\\:\\/\\/", "", repourl),
+                    "](", repourl, "), submitted to [",
+                    repo, "#", issue_id, "](https://github.com/",
+                    repo, "/issues/", issue_id, ")")
+    label <- "bug"
+
+    # Pasting comments straight to `gh --body` fails with any internally nested
+    # single- or double-quotes, but all works well when read from file, so:
+    f <- tempfile (fileext = ".txt")
+    writeLines (body, f)
+
+    args <- list ("issue",
+                  "create",
+                  "--repo", repo,
+                  "--title", title,
+                  "--body-file", f,
+                  "--label", label)
+
+    # This returns the URL of the opened issue
+    u <- system2 ("gh", args = args, stdout = TRUE, wait = TRUE)
+
+    return (u)
 }
