cc62380e7610e901f0d4f1f3be2ebf240aa13878
mpadge <mark.padgham@email.com>
2021-10-04 10:53:40 +0200

    add COMPOSE_FILE envvar to restrart.sh

--- restart.sh
+++ restart.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# https://docs.docker.com/engine/reference/commandline/compose/
+# - this envvar is automatically detected when building
+COMPOSE_FILE=/<path>/<to>/<this>/<project>/docker-compose.yml
+
 docker-compose down
 docker pull mpadge/pkgcheck
 docker build --no-cache -t roreviewapi .
