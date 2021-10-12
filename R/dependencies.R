09f71639278b6c4f90d436aee133532a1ee9aa6b
mpadge <mark.padgham@email.com>
2021-10-12 12:45:46 +0200

    return any uninstallable pkgs from pkgrep_install_deps for #19

--- R/dependencies.R
+++ R/dependencies.R
@@ -3,6 +3,8 @@
 #'
 #' @param path Path to local package
 #' @inheritParams serve_api
+#' @return Hopefully a character vector of length zero, otherwise a list of any
+#' R packages unable to be installed.
 #' @export
 pkgrep_install_deps <- function (path, os, os_release) {
 
@@ -14,4 +16,10 @@ pkgrep_install_deps <- function (path, os, os_release) {
     remotes::install_deps (pkgdir = path,
                            dependencies = TRUE,
                            upgrade = "always")
+
+    deps <- remotes::dev_package_deps (pkgdir = path,
+                                       dependencies = TRUE)
+    ip <- data.frame (installed.packages ())
+    
+    return (deps$package [which (!deps$package %in% ip$Package)])
 }
