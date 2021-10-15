0c93b8057a4ab53a24a2c6c12fdd56fbc1788587
mpadge <mark.padgham@email.com>
2021-10-15 14:01:40 +0200

    spaceout R/api.R

--- R/api.R
+++ R/api.R
@@ -15,22 +15,23 @@
 #'
 #' @family main
 #' @export
-serve_api <- function(
-                      port = 8000L,
-                      cache_dir = NULL,
-                      os = "",
-                      os_release = "") {
+serve_api <- function (port = 8000L,
+                       cache_dir = NULL,
+                       os = "",
+                       os_release = "") {
 
-    if (os != "")
-        Sys.setenv ("ROREVIEWAPI_OS" = os)
-    if (os_release != "")
-        Sys.setenv ("ROREVIEWAPI_OS_RELEASE" = os_release)
+    if (os != "") {
+          Sys.setenv ("ROREVIEWAPI_OS" = os)
+      }
+    if (os_release != "") {
+          Sys.setenv ("ROREVIEWAPI_OS_RELEASE" = os_release)
+      }
 
-    ip <- data.frame (utils::installed.packages())
+    ip <- data.frame (utils::installed.packages ())
 
-    f <- file.path(
-                   ip$LibPath [ip$Package == "roreviewapi"],
-                   "roreviewapi", "plumber.R"
+    f <- file.path (
+        ip$LibPath [ip$Package == "roreviewapi"],
+        "roreviewapi", "plumber.R"
     )
 
     # ----------cache_dir set up----------
@@ -39,8 +40,9 @@ serve_api <- function(
         requireNamespace ("pkgcheck")
 
         cache_dir <- ifelse ("PKGCHECK_CACHE_DIR" %in% names (Sys.getenv ()),
-                             Sys.getenv ("PKGCHECK_CACHE_DIR"),
-                             file.path (rappdirs::user_cache_dir (), "pkgcheck"))
+            Sys.getenv ("PKGCHECK_CACHE_DIR"),
+            file.path (rappdirs::user_cache_dir (), "pkgcheck")
+        )
 
         if (!file.exists (cache_dir)) {
             dir.create (cache_dir, recursive = TRUE)
@@ -52,44 +54,48 @@ serve_api <- function(
     # ----------log_dir set up----------
     log_dir <- here::here ("logs")
     Sys.setenv ("ROREVAPI_LOG_DIR" = log_dir)
-    if (!fs::dir_exists (log_dir))
-        fs::dir_create (log_dir)
+    if (!fs::dir_exists (log_dir)) {
+          fs::dir_create (log_dir)
+      }
 
     log_file <- tempfile ("roreviewapi_", log_dir, ".log")
     Sys.setenv ("ROREVAPI_LOG_FILE" = log_file)
     logger::log_appender (logger::appender_tee (log_file))
 
-    convert_empty <- function(string) {
+    convert_empty <- function (string) {
         ifelse (string == "", "-", string)
     }
 
     # ----------local static dir set up----------
     static_dir <- file.path (cache_dir, "static")
-    if (!file.exists (static_dir))
-        dir.create (static_dir, recursive = TRUE)
+    if (!file.exists (static_dir)) {
+          dir.create (static_dir, recursive = TRUE)
+      }
 
     # ----------plumber process set up----------
     pr <- plumber::pr (f)
 
-    pr <- plumber::pr_static(pr, "/assets", static_dir)
+    pr <- plumber::pr_static (pr, "/assets", static_dir)
 
-    #pr$registerHooks(
-    pr <- plumber::pr_hooks (pr,
-      list(
-        preroute = function() {
-          # Start timer for log info
-          tictoc::tic()
-        },
-        postroute = function(req, res) {
-          end <- tictoc::toc(quiet = TRUE)          # nolint
-          # Log details about the request and the response
-          # TODO: Sanitize log details - perhaps in convert_empty
-          logger::log_info('{convert_empty(req$REMOTE_ADDR)} "{convert_empty(req$HTTP_USER_AGENT)}" {convert_empty(req$HTTP_HOST)} {convert_empty(req$REQUEST_METHOD)} {convert_empty(req$PATH_INFO)} {convert_empty(req$QUERY_STRING)} {convert_empty(res$status)} {round(end$toc - end$tic, digits = getOption("digits", 5))}')          # nolint
-        }
-      )
+    # pr$registerHooks(
+    pr <- plumber::pr_hooks (
+        pr,
+        list (
+            preroute = function () {
+                # Start timer for log info
+                tictoc::tic ()
+            },
+            postroute = function (req, res) {
+                end <- tictoc::toc (quiet = TRUE) # nolint
+                # Log details about the request and the response
+                # TODO: Sanitize log details - perhaps in convert_empty
+                logger::log_info ('{convert_empty(req$REMOTE_ADDR)} "{convert_empty(req$HTTP_USER_AGENT)}" {convert_empty(req$HTTP_HOST)} {convert_empty(req$REQUEST_METHOD)} {convert_empty(req$PATH_INFO)} {convert_empty(req$QUERY_STRING)} {convert_empty(res$status)} {round(end$toc - end$tic, digits = getOption("digits", 5))}') # nolint
+            }
+        )
     )
 
     plumber::pr_run (pr,
-                     host = "0.0.0.0",
-                     port = as.integer (port))
+        host = "0.0.0.0",
+        port = as.integer (port)
+    )
 }
