40393edbe25eea8d9e7ce7693cce26e4071947e9
mpadge <mark.padgham@email.com>
2021-10-15 14:02:29 +0200

    spaceout R/dependencies.R

--- R/dependencies.R
+++ R/dependencies.R
@@ -9,19 +9,25 @@
 #' @export
 pkgrep_install_deps <- function (path, os, os_release) {
 
-    sysreq <- remotes::system_requirements (os = os,
-                                            os_release = os_release,
-                                            path = path)
+    sysreq <- remotes::system_requirements (
+        os = os,
+        os_release = os_release,
+        path = path
+    )
     tmp <- lapply (sysreq, system) # nolint
 
-    remotes::install_deps (pkgdir = path,
-                           dependencies = TRUE,
-                           upgrade = "always")
+    remotes::install_deps (
+        pkgdir = path,
+        dependencies = TRUE,
+        upgrade = "always"
+    )
 
-    deps <- remotes::dev_package_deps (pkgdir = path,
-                                       dependencies = TRUE)
+    deps <- remotes::dev_package_deps (
+        pkgdir = path,
+        dependencies = TRUE
+    )
     ip <- data.frame (utils::installed.packages ())
-    
+
     deps <- deps$package [which (!deps$package %in% ip$Package)]
     if (length (deps) > 0L) {
         install_dev_deps (deps)
@@ -48,27 +54,32 @@ install_dev_deps <- function (deps) {
 
         # Make sure no previous deps have also installed 'p':
         ip <- data.frame (utils::installed.packages ())
-        if (p %in% ip$Package)
-            next
+        if (p %in% ip$Package) {
+              next
+          }
 
         u <- paste0 (u_base, p)
         x <- rvest::read_html (u)
         dat <- rvest::html_table (x)
-        remote <- lapply (dat, function (i)
-                          i [grep ("^(URL|BugReports)\\:", i$X1), ])
+        remote <- lapply (dat, function (i) {
+              i [grep ("^(URL|BugReports)\\:", i$X1), ]
+          })
         remote <- do.call (rbind, remote)$X2
         remote <- gsub ("\\s*", "", unlist (strsplit (remote, ",")))
         remote <- grep ("^https\\:\\/\\/github\\.com", remote, value = TRUE)
         remote <- unique (gsub ("issues(\\/?)$", "", remote)) # BugReports URLs
 
-        if (length (remote) > 1L)
-            remote <- remote [1L]
+        if (length (remote) > 1L) {
+              remote <- remote [1L]
+          }
 
         if (length (remote) > 0L) {
             tryCatch (remotes::install_github (remote,
-                                               dependencies = TRUE,
-                                               upgrade = "never"),
-                      error = function (e) NULL)
+                dependencies = TRUE,
+                upgrade = "never"
+            ),
+            error = function (e) NULL
+            )
         } else {
             # previous entries in `deps` may make standard installation
             # possible:
