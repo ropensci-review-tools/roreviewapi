54fa1f8631a8cddce2a9494c88419b675a822306
mpadge <mark.padgham@email.com>
2021-11-18 14:41:40 +0100

    nope, don't need r-ubuntu; it's r-bpsm via pkgcheck [ci skip]

--- restart.sh
+++ restart.sh
@@ -6,7 +6,6 @@ COMPOSE_FILE=$PROJDIR/docker-compose.yml
 
 docker-compose -f $COMPOSE_FILE down
 docker pull nginx
-docker pull rocker/r-ubuntu
 docker pull mpadge/pkgcheck
 docker build --rm -f $DOCKERFILE --no-cache -t roreviewapi $PROJDIR/
 docker-compose -f $COMPOSE_FILE build
