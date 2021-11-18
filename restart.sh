f5c42bf00322bdbae242a395a6d1cd881850592d
mpadge <mark.padgham@email.com>
2021-11-18 14:40:06 +0100

    pull r-ubuntu + nginx on restart [ci skip]

--- restart.sh
+++ restart.sh
@@ -5,6 +5,8 @@ DOCKERFILE=$PROJDIR/Dockerfile
 COMPOSE_FILE=$PROJDIR/docker-compose.yml
 
 docker-compose -f $COMPOSE_FILE down
+docker pull nginx
+docker pull rocker/r-ubuntu
 docker pull mpadge/pkgcheck
 docker build --rm -f $DOCKERFILE --no-cache -t roreviewapi $PROJDIR/
 docker-compose -f $COMPOSE_FILE build
