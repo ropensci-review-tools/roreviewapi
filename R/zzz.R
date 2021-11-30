a5ece499d1f0344ae74ca9a7b72b208ddef7f08d
mpadge <mark.padgham@email.com>
2021-11-30 16:56:30 +0100

    translate pkgcheck cache dir option to envvar .onLoad

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,16 @@
 
     requireNamespace ("pkgcheck") # to load pkgcheck_cache_dir
 
+    cdir <- getOption ("pkgcheck.cache_dir")
+    if (!is.null (cdir)) {
+        Sys.setenv ("PKGCHECK_CACHE_DIR" = cdir)
+    }
+
     invisible ()
+}
+
+.onUnload <- function (libname, pkgname) { # nolint
+
+    Sys.unsetenv ("PKGCHECK_CACHE_DIR")
 }
 # nocov end
