f5235e06576982c17189b323fd58e3af29aa60e8
mpadge <mark.padgham@email.com>
2022-01-12 10:59:35 +0100

    add docker system prune to restart.sh

--- restart.sh
+++ restart.sh
@@ -5,8 +5,8 @@ DOCKERFILE=$PROJDIR/Dockerfile
 COMPOSE_FILE=$PROJDIR/docker-compose.yml
 
 docker-compose -f $COMPOSE_FILE down
-docker pull nginx
-docker pull mpadge/pkgcheck
+docker-compose pull
 docker build --rm -f $DOCKERFILE --no-cache -t roreviewapi $PROJDIR/
 docker-compose -f $COMPOSE_FILE build
 docker-compose -f $COMPOSE_FILE up -d
+docker system prune -f
