2f5146c51b5740dbaeebe500fa5f0a751724697f
mpadge <mark.padgham@email.com>
2022-06-17 16:59:53 +0200

    need --no-cache on docker-compose too [ci skip]

--- restart.sh
+++ restart.sh
@@ -7,7 +7,7 @@ COMPOSE_FILE=$PROJDIR/docker-compose.yml
 docker-compose -f $COMPOSE_FILE down
 docker pull mpadge/pkgcheck
 docker-compose pull
-docker build --rm -f $DOCKERFILE --no-cache -t roreviewapi $PROJDIR/
-docker-compose -f $COMPOSE_FILE build
+docker build --rm -f $DOCKERFILE --force-rm --no-cache -t roreviewapi $PROJDIR/
+docker-compose --force-rm --no-cache -f $COMPOSE_FILE build
 docker-compose -f $COMPOSE_FILE up -d
 docker system prune -f
