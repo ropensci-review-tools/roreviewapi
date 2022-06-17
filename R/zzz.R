b51446ce78677dbfd331f59d2c83e485961ebf81
mpadge <mark.padgham@email.com>
2022-06-17 11:11:40 +0200

    add R/zzz.R from pkgcheck to force cache_dir set here too

---
+++ R/zzz.R
@@ -0,0 +1,28 @@
+# nocov start
+.onLoad <- function (libname, pkgname) { # nolint
+
+    cache_dir <- Sys.getenv ("PKGCHECK_CACHE_DIR")
+
+    if (!nzchar (cache_dir)) {
+        cache_dir <- fs::path_expand (fs::path (
+            rappdirs::user_cache_dir (),
+            "R",
+            "pkgcheck"
+        ))
+        if (grepl ("^\\/root", cache_dir)) {
+            cache_dir <- gsub ("^\\/root", "/home", cache_dir)
+        }
+        Sys.setenv ("PKGCHECK_CACHE_DIR" = cache_dir)
+        Sys.setenv ("PKGCHECK_CACHE_DIR_UNSET" = "true")
+    }
+}
+
+.onUnload <- function (libname, pkgname) { # nolint
+
+    if (Sys.getenv ("PKGCHECK_CACHE_DIR_UNSET") == "true") {
+
+        Sys.unsetenv ("PKGCHECK_CACHE_DIR")
+        Sys.unsetenv ("PKGCHECK_CACHE_DIR_UNSET")
+    }
+}
+# nocov end
