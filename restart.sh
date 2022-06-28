9f6c3980ccc7a5e65cc589b0fce8ac09ca85611b
mpadge <mark.padgham@email.com>
2022-06-28 11:09:20 +0200

    add comment to restart script [ci skip]

--- restart.sh
+++ restart.sh
@@ -7,6 +7,7 @@ COMPOSE_FILE=$PROJDIR/docker-compose.yml
 docker-compose -f $COMPOSE_FILE down
 docker pull mpadge/pkgcheck
 docker-compose pull
+# also need to rebuild roreviewapi container for testing:
 docker build --rm -f $DOCKERFILE --force-rm --no-cache -t roreviewapi $PROJDIR/
 docker-compose -f $COMPOSE_FILE build --force-rm --no-cache
 docker-compose -f $COMPOSE_FILE up -d
