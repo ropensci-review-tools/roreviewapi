009dedac62bff8f5c9a747659cb22dbab5970a5b
mpadge <mark.padgham@email.com>
2022-08-16 10:50:30 +0200

    fix one docker-compose line in restart.sh [ci skip]

--- restart.sh
+++ restart.sh
@@ -6,7 +6,7 @@ COMPOSE_FILE=$PROJDIR/docker-compose.yml
 
 docker-compose -f $COMPOSE_FILE down
 docker pull mpadge/pkgcheck
-docker-compose pull
+docker-compose -f $COMPOSE_FILE pull
 # also need to rebuild roreviewapi container for testing:
 docker build --rm -f $DOCKERFILE --force-rm --no-cache -t roreviewapi $PROJDIR/
 docker-compose -f $COMPOSE_FILE build --force-rm --no-cache
