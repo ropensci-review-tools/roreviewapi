a2e4a509ee46f2731017987ad72ea04f97d8868b
Maëlle Salmon <maelle.salmon@yahoo.se>
2022-12-02 14:34:47 +0100

    improve

--- R/peer-review-badge.R
+++ R/peer-review-badge.R
@@ -1,11 +1,12 @@
 #' Check whether README.md features an rOpenSci software-review badge
 #'
-#' @param path
+#' @param path Path to package source
+#' @param issue_number Number of software review submission issue
 #'
 #' @return A string, empty if the badge was found.
 #' @export
 #'
-has_readme_badge <- function(path = getwd()) {
+has_readme_badge <- function(path = getwd(), issue_number = NULL) {
   readme_path <- file.path(path, "README.md")
   if (!file.exists(readme_path)) {
     stop(sprintf("Can't find README.md at %s", path))
@@ -23,31 +24,45 @@ has_readme_badge <- function(path = getwd()) {
     .[xml2::xml_name(.) == "image"] %>%
     xml2::xml_attr("destination")
 
-  is_ropensci_badge <- function(badges_links) {
-    grepl(
-      "https://badges.ropensci.org/[[:digit:]]*_status.svg",
-      badges_links,
-      perl = TRUE
-    )
+  is_ropensci_badge <- function(badges_links, issue_number) {
+    if (is.null(issue_number)) {
+      grepl(
+        "https://badges.ropensci.org/[[:digit:]]*_status.svg",
+        badges_links,
+        perl = TRUE
+      )
+    } else {
+      badges_links == sprintf("https://badges.ropensci.org/%s_status.svg", issue_number)
+    }
   }
 
-  is_http_ropensci_badge <- function(badges_links) {
-    grepl(
-      "http://badges.ropensci.org/[[:digit:]]*_status.svg",
-      badges_links,
-      perl = TRUE
-    )
+  is_http_ropensci_badge <- function(badges_links, issue_number) {
+    if (is.null(issue_number)) {
+      grepl(
+        "http://badges.ropensci.org/[[:digit:]]*_status.svg",
+        badges_links,
+        perl = TRUE
+      )
+    } else {
+      badges_links == sprintf("http://badges.ropensci.org/%s_status.svg", issue_number)
+    }
   }
 
-  if (!any(is_ropensci_badge(badges_links))) {
-    if (any(is_http_ropensci_badge(badges_links))) {
+  if (!any(is_ropensci_badge(badges_links, issue_number))) {
+    if (any(is_http_ropensci_badge(badges_links, issue_number))) {
       "Use https not http in the software review badge."
     } else {
+      if (is.null(issue_number)) {
+        issue_number <- "<issue-number>"
+      }
       paste(
         c(
           "Missing software review badge, add it with", "",
           "```markdown",
-          "[![Status at rOpenSci Software Peer Review](https://badges.ropensci.org/<issue-number>_status.svg)](https://github.com/ropensci/software-review/issues/<issue-number>)",
+          sprintf(
+            "[![Status at rOpenSci Software Peer Review](https://badges.ropensci.org/%s_status.svg)](https://github.com/ropensci/software-review/issues/%s)",
+            issue_number, issue_number
+          ),
           "```"
         ),
         collapse = "\n"
