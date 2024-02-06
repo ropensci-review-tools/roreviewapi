2366b4267fb06eba38d2a1c090c38a8d51cff394
mpadge <mark.padgham@email.com>
2024-02-06 11:36:34 +0100

    add --no-cache flag to docker build

--- restart.sh
+++ restart.sh
@@ -8,7 +8,7 @@ docker-compose -f $COMPOSE_FILE down
 docker pull mpadge/pkgcheck
 docker-compose -f $COMPOSE_FILE pull
 # also need to rebuild roreviewapi container for testing:
-docker build --rm -f $DOCKERFILE --force-rm --no-cache -t roreviewapi $PROJDIR/
+docker build --no-cache --rm -f $DOCKERFILE --force-rm --no-cache -t roreviewapi $PROJDIR/
 # system prune should be run when compose is stopped:
 docker system prune --all --volumes --force
 docker-compose -f $COMPOSE_FILE build --force-rm --no-cache
