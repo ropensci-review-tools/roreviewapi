ff219c2ec0d6e2ba852db843c4d4a750eec793a6
mpadge <mark.padgham@email.com>
2026-04-24 11:33:02 +0200

    simplify restart.sh for #94

--- restart.sh
+++ restart.sh
@@ -5,10 +5,6 @@ DOCKERFILE=$PROJDIR/Dockerfile
 COMPOSE_FILE=$PROJDIR/docker-compose.yml
 
 docker-compose -f $COMPOSE_FILE down
-docker-compose -f $COMPOSE_FILE pull
-# also need to rebuild roreviewapi container for testing:
-docker build --no-cache --rm -f $DOCKERFILE --force-rm --no-cache -t roreviewapi $PROJDIR/
-# system prune should be run when compose is stopped:
 docker system prune --all --volumes --force
 docker-compose -f $COMPOSE_FILE build --force-rm --no-cache
 docker-compose -f $COMPOSE_FILE up -d
